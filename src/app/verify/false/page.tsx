15eb834191347aec03fd6e4fd38f1e7951af5407
Mochamad Syahrial Alzaidan <88835497+syahrialalzaidan@users.noreply.github.com>
2024-07-12 23:01:09 +0700

    feat: added verify pages

--- src/app/verify/false/page.tsx
+++ src/app/verify/false/page.tsx
@@ -2,22 +2,22 @@ import { IoIosLock } from "react-icons/io";
 
 export default function VerifyTruePage() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <div className="shadow-merah bg-merah/50 relative mt-4 flex aspect-square w-80 items-center justify-center self-center overflow-hidden rounded-full shadow-2xl">
-        <div className="shadow-merah bg-merah/30 flex aspect-square w-64 items-center justify-center self-center rounded-full shadow-lg">
-          <div className="shadow-merah bg-merah/60 flex aspect-square w-48 items-center justify-center self-center rounded-full shadow-lg">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-merah/10">
+      <div className="relative mt-4 flex aspect-square w-80 items-center justify-center self-center overflow-hidden rounded-full bg-merah/50 shadow-2xl shadow-merah">
+        <div className="flex aspect-square w-64 items-center justify-center self-center rounded-full bg-merah/30 shadow-lg shadow-merah">
+          <div className="flex aspect-square w-48 items-center justify-center self-center rounded-full bg-merah/60 shadow-lg shadow-merah">
             <IoIosLock className="text-6xl text-white" />
           </div>
         </div>
         {/* Circles for animation */}
         <div className="flex items-center justify-center">
-          <div className="animate-expand border-merah absolute inset-0 aspect-square rounded-full border-4 opacity-75"></div>
-          <div className="animate-expand animation-delay-1 border-merah absolute inset-0 aspect-square rounded-full border-4 opacity-75"></div>
-          <div className="animate-expand animation-delay-2 border-merah absolute inset-0 aspect-square rounded-full border-4 opacity-75"></div>
+          <div className="animate-expand absolute inset-0 aspect-square rounded-full border-4 border-merah opacity-75"></div>
+          <div className="animate-expand animation-delay-1 absolute inset-0 aspect-square rounded-full border-4 border-merah opacity-75"></div>
+          <div className="animate-expand animation-delay-2 absolute inset-0 aspect-square rounded-full border-4 border-merah opacity-75"></div>
         </div>
       </div>
 
-      <div className="flex flex-col gap-2 mt-14">
+      <div className="mt-14 flex flex-col gap-2">
         <h1 className="text-center text-4xl font-semibold">Danger Detected</h1>
         <p className="text-center">We'll be here until you're safe</p>
       </div>
