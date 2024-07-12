6dedc7deb2750b2d6c45d134df445758b42268fa
Mochamad Syahrial Alzaidan <88835497+syahrialalzaidan@users.noreply.github.com>
2024-07-13 02:03:48 +0700

    feat: updated verify

--- src/app/components/HomeButton.tsx
+++ src/app/components/HomeButton.tsx
@@ -3,7 +3,9 @@ import { MdMoreHoriz, MdHistory } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
 import { CiSettings } from "react-icons/ci";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 interface SafeProps {
   setSafe: React.Dispatch<React.SetStateAction<boolean>>;
@@ -16,14 +18,24 @@ export default function Home() {
 }
 
 function Danger({ setSafe }: SafeProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/verify");
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
-    <div className="bg-ijo-muda/50 flex min-h-screen items-center justify-center">
+    <div className="flex min-h-screen items-center justify-center bg-ijo-muda/50">
       <div
         onClick={() => setSafe(true)}
-        className="shadow-ijo-muda relative mt-4 flex aspect-square w-80 items-center justify-center self-center overflow-hidden rounded-full bg-white shadow-2xl"
+        className="relative mt-4 flex aspect-square w-80 items-center justify-center self-center overflow-hidden rounded-full bg-white shadow-2xl shadow-ijo-muda"
       >
-        <div className="shadow-ijo-muda flex aspect-square w-64 items-center justify-center self-center rounded-full bg-white shadow-lg">
-          <div className="shadow-ijo-muda flex aspect-square w-48 items-center justify-center self-center rounded-full bg-white shadow-lg">
+        <div className="flex aspect-square w-64 items-center justify-center self-center rounded-full bg-white shadow-lg shadow-ijo-muda">
+          <div className="flex aspect-square w-48 cursor-pointer items-center justify-center self-center rounded-full bg-white shadow-lg shadow-ijo-muda">
             <p>
               <strong>Stay Safe</strong>
             </p>
@@ -31,9 +43,9 @@ function Danger({ setSafe }: SafeProps) {
         </div>
         {/* Circles for animation */}
         <div className="flex items-center justify-center">
-          <div className="animate-expand border-ijo-muda absolute inset-0 aspect-square rounded-full border-4 opacity-75"></div>
-          <div className="animate-expand animation-delay-1 border-ijo-muda absolute inset-0 aspect-square rounded-full border-4 opacity-75"></div>
-          <div className="animate-expand animation-delay-2 border-ijo-muda absolute inset-0 aspect-square rounded-full border-4 opacity-75"></div>
+          <div className="animate-expand absolute inset-0 aspect-square rounded-full border-4 border-ijo-muda opacity-75"></div>
+          <div className="animate-expand animation-delay-1 absolute inset-0 aspect-square rounded-full border-4 border-ijo-muda opacity-75"></div>
+          <div className="animate-expand animation-delay-2 absolute inset-0 aspect-square rounded-full border-4 border-ijo-muda opacity-75"></div>
         </div>
       </div>
     </div>
@@ -44,29 +56,32 @@ function Safe({ setSafe }: SafeProps) {
   return (
     <div className="flex min-h-screen flex-col gap-8 bg-[url('/start-left.svg')] bg-no-repeat px-[10%] py-16">
       <div className="flex justify-between">
-        <div className="border-biru w-fit rounded-full border bg-white p-3 shadow-md">
+        <div className="w-fit rounded-full border border-biru bg-white p-3 shadow-md">
           <MdMoreHoriz className="text-2xl" />
         </div>
-        <div className="border-biru w-fit rounded-full border bg-white p-3 shadow-md">
+        <div className="w-fit rounded-full border border-biru bg-white p-3 shadow-md">
           <CgProfile className="text-2xl" />
         </div>
       </div>
       <div className="flex justify-between">
-        <div className="border-biru flex w-fit gap-4 rounded-full border bg-white p-3 shadow-md">
+        <div className="flex w-fit gap-4 rounded-full border border-biru bg-white p-3 shadow-md">
           <CiSettings className="text-2xl" />
           <p>Settings</p>
         </div>
-        <div className="border-biru w-fit rounded-full border bg-white p-3 shadow-md">
+        <Link
+          href={"/watch"}
+          className="w-fit rounded-full border border-biru bg-white p-3 shadow-md"
+        >
           <Image
             src={"/band-outline.svg"}
             alt="My Band"
             width={28}
             height={25}
           />
-        </div>
+        </Link>
       </div>
       <div className="flex justify-between">
-        <div className="border-biru flex w-fit gap-4 rounded-full border bg-white p-3 shadow-md">
+        <div className="flex w-fit gap-4 rounded-full border border-biru bg-white p-3 shadow-md">
           <MdHistory className="text-2xl" />
           <p>History</p>
         </div>
@@ -75,9 +90,9 @@ function Safe({ setSafe }: SafeProps) {
       {/* Button Section */}
       <div
         onClick={() => setSafe(false)}
-        className="mt-4 flex aspect-square w-80 items-center justify-center self-center rounded-full bg-white shadow-md"
+        className="flex aspect-square w-80 items-center justify-center self-center rounded-full bg-white shadow-md"
       >
-        <div className="shadow-biru flex aspect-square w-48 items-center justify-center self-center rounded-full bg-white shadow-lg">
+        <div className="flex aspect-square w-48 cursor-pointer items-center justify-center self-center rounded-full bg-white shadow-lg shadow-biru">
           <p>
             Hold to <strong>Start</strong>
           </p>
@@ -86,7 +101,7 @@ function Safe({ setSafe }: SafeProps) {
 
       {/* Foot Section */}
       <div className="flex items-center justify-end gap-2">
-        <div className="border-secondary shadow-secondary w-fit rounded-lg border px-4 py-2 shadow-sm">
+        <div className="w-fit rounded-lg border border-secondary px-4 py-2 shadow-sm shadow-secondary">
           <p>
             Hi, <strong>Gibeh</strong> 👋
           </p>
